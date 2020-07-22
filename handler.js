ed3a1222ca389713982c960565da50612cd5bb1b
trido2359 <tri.do@2359media.com>
2020-07-22 16:23:15 +0700

    test

---
+++ handler.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports.test = async event => {
+  return {
+    statusCode: 200,
+    body: 'Test'
+  };
+
+};
+
+module.exports.hello = async event => {
+  return {
+    statusCode: 200,
+    body: 'Hello'
+  }
+  
+};
