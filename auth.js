6bdc5040d2501064ccd5fcde3ea195551920cd38
trido2359 <tri.do@2359media.com>
2020-07-27 23:48:33 +0700

    update9

--- auth.js
+++ auth.js
@@ -1,6 +1,8 @@
 
 module.exports.authorize =  (event, context, cb ) => {
     const check = true;
+    console.log('authorize');
+    
     if (check) {
         cb('Unauthorized11');
     }
--- handler.js
+++ handler.js
@@ -19,6 +19,8 @@ module.exports.test = async event => {
 };
 
 module.exports.hello =  async event => {
+    console.log('hello'); 
+    console.log(event);
     try {
         return {
             statusCode: 200,
