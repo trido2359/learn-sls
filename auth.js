36ea771b18ca14533e90ba71fe30c50c0761e963
trido2359 <tri.do@2359media.com>
2020-07-29 09:55:26 +0700

    update1

--- auth.js
+++ auth.js
@@ -1,20 +1,27 @@
 
+const generatePolicy = (principalId, effect, resource) => {
+    const authResponse = {};
+    authResponse.principalId = principalId;
+    if (effect && resource) {
+      const policyDocument = {};
+      policyDocument.Version = '2012-10-17';
+      policyDocument.Statement = [];
+      const statementOne = {};
+      statementOne.Action = 'execute-api:Invoke';
+      statementOne.Effect = effect;
+      statementOne.Resource = resource;
+      policyDocument.Statement[0] = statementOne;
+      authResponse.policyDocument = policyDocument;
+    }
+    return authResponse;
+  };
+
 module.exports.authorize = async (event, context, cb) => {
     console.log('authorize');
     // return policy statement that allows to invoke the connect function.
     // in a real world application, you'd verify that the header in the event
     // object actually corresponds to a user, and return an appropriate statement accordingly
-    return {
-        "principalId": "user",
-        "policyDocument": {
-            "Version": "2012-10-17",
-            "Statement": [
-                {
-                    "Action": "execute-api:Invoke",
-                    "Effect": "Allow",
-                    "Resource": "arn:aws:execute-api:ap-southeast-1:890191265537:cbn7bpny75/*/GET/"
-                }
-            ]
-        }
-    };
+    const resource = "arn:aws:execute-api:ap-southeast-1:890191265537:cbn7bpny75/*/GET/";
+    const policy = generatePolicy('user', 'Allow', resource);
+    return policy;
 }
