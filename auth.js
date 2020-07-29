e968e00f4345b0a419d0c2d0a3200bc714b8354e
trido2359 <tri.do@2359media.com>
2020-07-29 10:10:56 +0700

    update2

--- auth.js
+++ auth.js
@@ -17,7 +17,7 @@ const generatePolicy = (principalId, effect, resource) => {
   };
 
 module.exports.authorize = async (event, context, cb) => {
-    console.log('authorize');
+    console.log('authorize11');
     // return policy statement that allows to invoke the connect function.
     // in a real world application, you'd verify that the header in the event
     // object actually corresponds to a user, and return an appropriate statement accordingly
