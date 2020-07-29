4068651c178f77474c44e3875e22a6703211547b
trido2359 <tri.do@2359media.com>
2020-07-29 08:12:36 +0700

    update2

--- auth.js
+++ auth.js
@@ -12,7 +12,7 @@ module.exports.authorize = async (event, context, cb) => {
                 {
                     "Action": "execute-api:Invoke",
                     "Effect": "Allow",
-                    "Resource": "arn:aws:execute-api:ap-southeast-1:890191265537: cbn7bpny75/*/GET/"
+                    "Resource": "arn:aws:execute-api:ap-southeast-1:890191265537:cbn7bpny75/*/GET/"
                 }
             ]
         }
